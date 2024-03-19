b7976a5673f58a278e611f18ef0e9fc3dfb4d23d
Subrata Shawon <96424696+Subrata1608024@users.noreply.github.com>
2024-03-19 06:19:38 +0600

    first commit

---
+++ src/test/java/TestSelenium/LaunchChrome.java
@@ -0,0 +1,17 @@
+package TestSelenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchChrome {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/downloads/");
+        Thread.sleep(3000);
+        driver.quit();
+
+
+    }
+}
---
+++ src/test/java/TestSelenium/LaunchFirefox.java
@@ -0,0 +1,17 @@
+package TestSelenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LaunchFirefox {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.selenium.dev/downloads/");
+        Thread.sleep(3000);
+
+        driver.quit();
+
+    }
+}
